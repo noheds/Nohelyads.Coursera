d736685ff955e2254ad7e94d7e888463503d8294
noheds <nohelya.dossantos@icloud.com>
2016-08-21 16:47:09 -0500

    Swift y IOS semana 2

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+
+
+//Nohelya Dos Santos
+
+import UIKit
+
+for var i = 1; i < 101; i++ {
+ 
+    
+    if i>30 && i<40  {
+        
+        print("# ", i , "Viva Swift")
+        
+    }else if  i%5 == 0 {
+        
+        print("# ", i, " Bingo")
+        
+    } else if i%2 == 0 {
+        
+       print("# ", i , " Par!!!")
+        
+    }else if i%2 != 0 {
+       
+        print("# ", i, "Impar!!!")
+    
+    }
+
+}
